c540a8fd342f9da6264f72cfbd99f79d4b631d4f
felix st-gelais <ciditworld@gmail.com>
2020-11-20 08:52:44 -0500

    week 3 report 19
    DONE
    in the same spirit as last commit, re-converted the repositories from ReactiveMongoRepositories to JpaRepositories

--- src/main/kotlin/cal/fstg/stagerkotlin/dao/DocumentRepositories.kt
+++ src/main/kotlin/cal/fstg/stagerkotlin/dao/DocumentRepositories.kt
@@ -2,8 +2,9 @@ package cal.fstg.stagerkotlin.dao
 
 import cal.fstg.stagerkotlin.model.Document
 import cal.fstg.stagerkotlin.model.Resume
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
-interface DocumentRepository : ReactiveMongoRepository<Document, String>
+interface DocumentRepository : JpaRepository<Document, String>
 
-interface ResumeRepository : ReactiveMongoRepository<Resume, String>
+interface ResumeRepository : JpaRepository<Resume, String>
