efb7c51da0752575baf5edbf7ed415e0dff42eb5
felix st-gelais <ciditworld@gmail.com>
2020-11-20 08:53:17 -0500

    week 3 report 19 (continued)
    DONE
    in the same spirit as last commit, re-converted the repositories from ReactiveMongoRepositories to JpaRepositories (i forgot the user repositories)

--- src/main/kotlin/cal/fstg/stagerkotlin/dao/UserRepositories.kt
+++ src/main/kotlin/cal/fstg/stagerkotlin/dao/UserRepositories.kt
@@ -4,12 +4,13 @@ import cal.fstg.stagerkotlin.model.Admin
 import cal.fstg.stagerkotlin.model.Employer
 import cal.fstg.stagerkotlin.model.Student
 import cal.fstg.stagerkotlin.model.User
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
-interface UserRepository : ReactiveMongoRepository<User, String>
+interface UserRepository : JpaRepository<User, String>
 
-interface StudentRepository : ReactiveMongoRepository<Student, String>
+interface StudentRepository : JpaRepository<Student, String>
 
-interface EmployerRepository : ReactiveMongoRepository<Employer, String>
+interface EmployerRepository : JpaRepository<Employer, String>
 
-interface AdminRepository : ReactiveMongoRepository<Admin, String>
+interface AdminRepository : JpaRepository<Admin, String>
